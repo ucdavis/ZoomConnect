27044ff0e44073a9788311844ce8c39f88155428
Edward Henn <emhenn@ucdavis.edu>
2020-07-07 11:52:17 -0700

    add solution and default asp.net core web project

---
+++ ZoomConnect.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZoomConnect.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
