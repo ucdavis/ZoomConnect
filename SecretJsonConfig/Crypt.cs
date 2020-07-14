d8bcee12d158eb6696676b91905b0ac7929fb63e
Edward Henn <emhenn@ucdavis.edu>
2020-07-14 10:25:29 -0700

    add crypt class to encrypt/decrypt plaintext using injected IDataProtectionProvider

---
+++ SecretJsonConfig/Crypt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace SecretJsonConfig
+{
+    public class Crypt
+    {
+        private readonly IDataProtector _protector;
+
+        public Crypt(IDataProtectionProvider provider)
+        {
+            _protector = provider.CreateProtector(GetType().FullName);
+        }
+
+        public string Encrypt(string plaintext)
+        {
+            return _protector.Protect(plaintext);
+        }
+
+        public bool TryDecrypt(string encryptedText, out string decryptedText)
+        {
+            try
+            {
+                decryptedText = _protector.Unprotect(encryptedText);
+
+                return true;
+            }
+            catch (CryptographicException)
+            {
+                decryptedText = null;
+
+                return false;
+            }
+        }
+    }
+}
