f07ee2b211dd2ada61233d0b9788a0d2eda99af4
Edward Henn <emhenn@ucdavis.edu>
2020-07-14 17:02:26 -0700

    remove unused code

--- ZoomConnect.Web/Models/ZoomOptions.cs
+++ ZoomConnect.Web/Models/ZoomOptions.cs
@@ -7,16 +7,12 @@ namespace ZoomConnect.Web.Models
 {
     public class ZoomOptions
     {
-        public const string Name = "ZoomSecrets";
-
         public ZoomOptions()
         {
-            //Creds = new List<GenericCredential>();
             Secret = new SecretStruct("");
         }
 
         public SecretStruct Secret { get; set; }
         public string NotSecret { get; set; }
-        //public List<GenericCredential> Creds { get; set; }
     }
 }
