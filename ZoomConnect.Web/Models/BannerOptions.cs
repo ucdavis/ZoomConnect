4b0e0eb2f3d2ab470c1f8e2d796d6cd4696235a1
Edward Henn <emhenn@ucdavis.edu>
2020-07-16 09:07:29 -0700

    add Banner config options

---
+++ ZoomConnect.Web/Models/BannerOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SecretJsonConfig;
+
+namespace ZoomConnect.Web.Models
+{
+    public class BannerOptions
+    {
+        public BannerOptions()
+        {
+            Username = new SecretStruct("");
+            Password = new SecretStruct("");
+        }
+
+        public string Instance { get; set; }
+        public SecretStruct Username { get; set; }
+        public SecretStruct Password { get; set; }
+
+        public string GetConnectionString()
+        {
+            return $"Data Source={Instance};User Id={Username};Password={Password};";
+        }
+    }
+}
--- ZoomConnect.Web/Models/ZoomOptions.cs
+++ ZoomConnect.Web/Models/ZoomOptions.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text.Json.Serialization;
-using SecretJsonConfig;
 
 namespace ZoomConnect.Web.Models
 {
     public class ZoomOptions
     {
-        public ZoomOptions()
-        {
-            Secret = new SecretStruct("");
-        }
-
-        public SecretStruct Secret { get; set; }
-        public string NotSecret { get; set; }
+        public BannerOptions Banner { get; set; }
     }
 }
