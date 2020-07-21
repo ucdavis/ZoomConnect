d21d061cce82c5152fef28694775f8441be1c7c7
Edward Henn <emhenn@ucdavis.edu>
2020-07-21 16:02:10 -0700

    manually add all repositories as generics; scrutor will not add partially closed generics

--- ZoomConnect.Web/Banner/BannerExtensions.cs
+++ ZoomConnect.Web/Banner/BannerExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ZoomConnect.Web.Banner;
+using ZoomConnect.Web.Banner.Domain;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -9,11 +10,21 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             services.AddScoped<BannerContext>();
 
-            services.Scan(scan => scan
-                .FromAssemblyOf<TestRepository>()
-                .AddClasses(classes => classes.AssignableTo(typeof(AbstractRepository<>)))
-                .AsSelf()
-                .WithScopedLifetime());
+            //services.Scan(scan => scan
+            //    .FromAssemblyOf<TestRepository>()
+            //    .AddClasses(classes => classes.AssignableTo(typeof(AbstractRepository<>)))
+            //    .AsSelf()
+            //    .WithScopedLifetime());
+
+            services.AddScoped<AbstractRepository<dual>, TestRepository>();
+            services.AddScoped<AbstractRepository<goremal>, GoremalRepository>();
+            services.AddScoped<AbstractRepository<sirasgn>, SirasgnRepository>();
+            services.AddScoped<AbstractRepository<sobcald>, SobcaldRepository>();
+            services.AddScoped<AbstractRepository<spriden>, SpridenRepository>();
+            services.AddScoped<AbstractRepository<ssbsect>, SsbsectRepository>();
+            services.AddScoped<AbstractRepository<ssrmeet>, SsrmeetRepository>();
+            services.AddScoped<AbstractRepository<stvsubj>, StvsubjRepository>();
+            services.AddScoped<AbstractRepository<stvterm>, StvtermRepository>();
 
             return services;
         }
