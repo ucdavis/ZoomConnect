0695b3c2ef0e32b976380f2094be86280b390eea
Edward Henn <emhenn@ucdavis.edu>
2020-07-22 10:32:40 -0700

    add RequirementType to group SetupRequirements by resource

---
+++ ZoomConnect.Web/SetupRequirements/RequirementType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ZoomConnect.Web.SetupRequirements
+{
+    public enum RequirementType
+    {
+        Banner,
+        Zoom,
+        Exchange,
+        Mediasite,
+        Canvas,
+        Box,
+        Photo
+    }
+}
