5c196bd21284b79c62c48390b9dad387a63ceab3
Edward Henn <emhenn@ucdavis.edu>
2020-07-24 09:28:17 -0700

    add datamodels for course and prof - representing one record with all related data

---
+++ ZoomConnect.Web/Models/CourseMeetingDataModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using ZoomClient.Domain;
+using ZoomConnect.Web.Banner.Domain;
+
+namespace ZoomConnect.Web.Models
+{
+    public class CourseMeetingDataModel
+    {
+        public CourseMeetingDataModel()
+        {
+        }
+
+        public ssrmeet bannerMeeting { get; set; }
+        public ssbsect bannerCourse { get; set; }
+        public spriden primaryProf { get; set; }
+        public List<spriden> otherProfs { get; set; }
+        // email?
+
+        public Meeting zoomMeeting { get; set; }
+    }
+}
---
+++ ZoomConnect.Web/Models/ProfDataModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using ZoomClient.Domain;
+using ZoomConnect.Web.Banner.Domain;
+
+namespace ZoomConnect.Web.Models
+{
+    /// <summary>
+    /// Represents one professor teaching classes in the currently selected term/subj.
+    /// Holds links to relevant rows in Banner, Zoom, etc.
+    /// </summary>
+    public class ProfDataModel
+    {
+        public ProfDataModel()
+        {
+        }
+
+        public spriden bannerPerson { get; set; }
+        public goremal primaryEmail { get; set; }
+        public List<goremal> otherEmails { get; set; }
+        public List<sirasgn> assignments { get; set; }
+
+        public User zoomUser { get; set; }
+    }
+}
