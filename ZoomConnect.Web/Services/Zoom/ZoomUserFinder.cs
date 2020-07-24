77d76df3798e9b3645d492d3be3a0431d726f564
Edward Henn <emhenn@ucdavis.edu>
2020-07-24 16:29:19 -0700

    fix userfinder- add spriden row; only add missing emails if they are not found or alternates

--- ZoomConnect.Web/Services/Zoom/ZoomUserFinder.cs
+++ ZoomConnect.Web/Services/Zoom/ZoomUserFinder.cs
@@ -12,14 +12,15 @@ namespace ZoomConnect.Web.Services.Zoom
     /// </summary>
     public class ZoomUserFinder
     {
+        private CachedRepository<spriden> _personRepo;
         private CachedRepository<goremal> _profEmailRepo;
         private ZoomClient.Zoom _zoomClient;
         private List<ProfDataModel> _foundProfs { get; set; }
         private List<goremal> _missingProfs { get; set; }
-        private List<goremal> _alternateEmails { get; set; }
 
-        public ZoomUserFinder(CachedRepository<goremal> profEmailRepo, ZoomClient.Zoom zoomClient)
+        public ZoomUserFinder(CachedRepository<spriden> personRepo, CachedRepository<goremal> profEmailRepo, ZoomClient.Zoom zoomClient)
         {
+            _personRepo = personRepo;
             _profEmailRepo = profEmailRepo;
             _zoomClient = zoomClient;
 
@@ -49,14 +50,19 @@ namespace ZoomConnect.Web.Services.Zoom
         }
 
         /// <summary>
-        /// Searches for Zoom User by each email in goremal
+        /// Searches for Zoom User by each email in goremal.
+        /// Looks for Preferred email first.
+        /// When found, all alternate emails are collated with found prof and not checked in Zoom.
         /// </summary>
         /// <returns></returns>
         private void Find()
         {
             _foundProfs = new List<ProfDataModel>();
             _missingProfs = new List<goremal>();
-            _alternateEmails = new List<goremal>();
+            var _allFound = new List<goremal>();
+
+            // find all person rows
+            var bannerPeople = _personRepo.GetAll();
 
             // find preferred emails first
             var allEmails = _profEmailRepo.GetAll()
@@ -66,31 +72,31 @@ namespace ZoomConnect.Web.Services.Zoom
             allEmails.ForEach(e =>
             {
                 // skip any emails for profs who are already found in Zoom
-                if (_alternateEmails.Contains(e)) { return; }
+                if (_allFound.Contains(e)) { return; }
 
                 // check Zoom for prof email
                 var zoomUser = _zoomClient.GetUser(e.email_address);
-                if (zoomUser != null)
-                {
-                    // create a new prof model with email connected to zoom
-                    var prof = new ProfDataModel
-                    {
-                        primaryEmail = e,
-                        zoomUser = zoomUser
-                    };
+                if (zoomUser == null) { return; }
 
-                    // record alternate emails so we can skip them later
-                    var altEmails = allEmails.Where(alt => alt.pidm == e.pidm && alt.email_address != e.email_address);
-                    prof.otherEmails.AddRange(altEmails);
-                    _alternateEmails.AddRange(altEmails);
-
-                    _foundProfs.Add(prof);
-                }
-                else
+                // create a new prof model with email connected to zoom
+                var prof = new ProfDataModel
                 {
-                    _missingProfs.Add(e);
-                }
+                    bannerPerson = bannerPeople.FirstOrDefault(p => p.pidm == e.pidm),
+                    primaryEmail = e,
+                    zoomUser = zoomUser
+                };
+
+                // get prof's alternate emails so we can skip them later
+                var altEmails = allEmails.Where(alt => alt.pidm == e.pidm && alt.email_address != e.email_address);
+                prof.otherEmails.AddRange(altEmails);
+                _allFound.Add(e);
+                _allFound.AddRange(altEmails);
+
+                _foundProfs.Add(prof);
             });
+
+            // missing
+            _missingProfs.AddRange(allEmails.Where(e => !_allFound.Contains(e)));
         }
     }
 }
