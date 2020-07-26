417d7e8cbec3102ad6c4280dd806472c46316de4
Edward Henn <emhenn@ucdavis.edu>
2020-07-26 06:27:15 -0700

    pull Profs list into its own Controller. Will handle prof creation etc. later

---
+++ ZoomConnect.Web/Controllers/ProfsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZoomConnect.Web.Banner.Cache;
+using ZoomConnect.Web.Banner.Domain;
+using ZoomConnect.Web.Filters;
+using ZoomConnect.Web.Services.Zoom;
+
+namespace ZoomConnect.Web.Controllers
+{
+    [Authorize]
+    [TypeFilter(typeof(CheckRequirements))]
+    public class ProfsController : Controller
+    {
+        private CachedRepository<goremal> _emailTable;
+        private CachedProfModels _userFinder;
+
+        public ProfsController(CachedRepository<goremal> emailTable, CachedProfModels userFinder)
+        {
+            _emailTable = emailTable;
+            _userFinder = userFinder;
+        }
+
+        public IActionResult Index()
+        {
+            var goremalRows = _emailTable.GetAll();
+            ViewData["GoremalCount"] = goremalRows.Count;
+
+            return View(_userFinder);
+        }
+    }
+}
