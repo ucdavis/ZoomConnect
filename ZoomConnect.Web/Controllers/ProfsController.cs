2a5510c786916bac971285001ffed659e50f3975
Edward Henn <emhenn@ucdavis.edu>
2020-07-28 14:43:56 -0700

    profs controller cleanup - goremal not needed

--- ZoomConnect.Web/Controllers/ProfsController.cs
+++ ZoomConnect.Web/Controllers/ProfsController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using ZoomConnect.Web.Banner.Cache;
-using ZoomConnect.Web.Banner.Domain;
 using ZoomConnect.Web.Filters;
 using ZoomConnect.Web.Services.Zoom;
 using ZoomConnect.Web.ViewModels;
@@ -15,20 +13,15 @@ namespace ZoomConnect.Web.Controllers
     [TypeFilter(typeof(CheckRequirements))]
     public class ProfsController : Controller
     {
-        private CachedRepository<goremal> _emailTable;
         private CachedProfModels _userFinder;
 
-        public ProfsController(CachedRepository<goremal> emailTable, CachedProfModels userFinder)
+        public ProfsController(CachedProfModels userFinder)
         {
-            _emailTable = emailTable;
             _userFinder = userFinder;
         }
 
         public IActionResult Index()
         {
-            var goremalRows = _emailTable.GetAll();
-            ViewData["GoremalCount"] = goremalRows.Count;
-
             var profModels = _userFinder.Profs.Select(p => new ProfViewModel(p))
                 .OrderBy(m => m.Name)
                 .ToList();
@@ -50,6 +43,5 @@ namespace ZoomConnect.Web.Controllers
 
             return View(profModels);
         }
-
     }
 }
