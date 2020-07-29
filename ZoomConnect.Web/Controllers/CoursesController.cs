38d8eb5ba238d788088828d6fd8388da8afac42e
Edward Henn <emhenn@ucdavis.edu>
2020-07-29 11:05:04 -0700

    update Courses list action and view to use viewmodel matching Profs

--- ZoomConnect.Web/Controllers/CoursesController.cs
+++ ZoomConnect.Web/Controllers/CoursesController.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using ZoomConnect.Web.Banner.Cache;
-using ZoomConnect.Web.Banner.Domain;
 using ZoomConnect.Web.Filters;
 using ZoomConnect.Web.Services.Zoom;
+using ZoomConnect.Web.ViewModels;
 
 namespace ZoomConnect.Web.Controllers
 {
@@ -13,19 +13,39 @@ namespace ZoomConnect.Web.Controllers
     public class CoursesController : Controller
     {
         private CachedMeetingModels _meetingModels;
-        private CachedRepository<ssrmeet> _meetingTable;
 
-        public CoursesController(CachedMeetingModels meetingModels, CachedRepository<ssrmeet> meetingTable)
+        public CoursesController(CachedMeetingModels meetingModels)
         {
             _meetingModels = meetingModels;
-            _meetingTable = meetingTable;
         }
 
         public IActionResult Index()
         {
-            ViewData["RowCount"] = _meetingTable.GetAll().Count;
+            var coursesModel = new SelectedCoursesViewModel
+            {
+                Courses = _meetingModels.Meetings
+                    .Select(m => new CourseViewModel(m))
+                    .OrderBy(m => m.Description)
+                    .ToList()
+            };
 
-            return View(_meetingModels);
+            return View(coursesModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(SelectedCoursesViewModel model)
+        {
+            var selectedMeetingIds = model.Courses
+                .Where(c => c.IsSelected)
+                .Select(c => c.MeetingId);
+
+            model.Courses = _meetingModels.Meetings
+                .Select(m => new CourseViewModel(m) { IsSelected = true })
+                .Where(c => selectedMeetingIds.Contains(c.MeetingId))
+                .OrderBy(c => c.Description)
+                .ToList();
+
+            return View(model);
         }
     }
 }
