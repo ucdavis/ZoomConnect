e19b92e88c16789c09442b7d8fc4b37fe1bfbe77
Edward Henn <emhenn@ucdavis.edu>
2020-07-28 18:08:55 -0700

    fix GetPlanUsage bug in zoom client.  List of PlanDetails returned for plan_webinar.
    API docs however state that plan_base is also returned as a list of PlanDetails
    but that is not the case when results are returned.

--- ZoomClient/Domain/Billing/PlanUsage.cs
+++ ZoomClient/Domain/Billing/PlanUsage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ZoomClient.Domain.Billing
 {
@@ -6,7 +7,7 @@ namespace ZoomClient.Domain.Billing
     {
         public PlanDetails plan_base { get; set; }
         public PlanRecording plan_recording { get; set; }
-        public PlanDetails plan_webinar { get; set; }
+        public List<PlanDetails> plan_webinar { get; set; }
         public PlanDetails plan_zoom_rooms { get; set; }
     }
 }
