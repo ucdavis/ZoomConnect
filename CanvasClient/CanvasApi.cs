b7580e19c93e4c78f83a0d80eafdd579c7cd4007
Edward Henn <emhenn@ucdavis.edu>
2020-07-30 15:52:07 -0700

    add initial CanvasClient project with RestSharp reference

---
+++ CanvasClient/CanvasApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RestSharp;
+
+namespace CanvasClient
+{
+    public class CanvasApi
+    {
+        private readonly string BaseUrl = "https://canvas.ucdavis.edu/api/v1/";
+        private RestClient client = null;
+
+        public CanvasApi()
+        {
+            client = new RestClient(BaseUrl);
+        }
+    }
+}
---
+++ CanvasClient/Domain/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CanvasClient.Domain
+{
+    public class Account
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string workflow_state { get; set; }
+        public int parent_account_id { get; set; }
+        public int root_account_id { get; set; }
+        public string uuid { get; set; }
+        public int default_storage_quota_mb { get; set; }
+        public int default_user_storage_quota_mb { get; set; }
+        public int default_group_storage_quota_mb { get; set; }
+        public string default_time_zone { get; set; }
+    }
+}
