b7fb34819a419bf740c1330b691f36df9a714a66
Edward Henn <emhenn@ucdavis.edu>
2020-07-31 10:00:09 -0700

    add canvas extension to extract Next url from Link header

---
+++ CanvasClient/Extensions/CanvasExtensions.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace CanvasClient.Extensions
+{
+    public static class CanvasExtensions
+    {
+        /// <summary>
+        /// Returns next page url extracted from Link header, otherwise null
+        /// </summary>
+        /// <param name="headers"></param>
+        /// <returns></returns>
+        public static string NextPageUrl(this IList<Parameter> headers)
+        {
+            var linkHeader = headers.FirstOrDefault(h => h.Name.ToLower() == "link");
+
+            if (linkHeader == null) { return null; }
+
+            var regex = new Regex("<([^>]+)>; rel=\"next\"", RegexOptions.IgnoreCase);
+
+            var match = regex.Match(linkHeader.Value.ToString());
+
+            if (match.Success) { return match.Groups[1].Value; }
+
+            return null;
+        }
+    }
+}
